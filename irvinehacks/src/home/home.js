f0e886ab1ba8f23e137760fd1885d1fed5f248f7
Lauren <LPSHU@uci.edu>
2024-01-27 10:16:35 -0800

    structure for App

---
+++ irvinehacks/src/home/home.js
@@ -0,0 +1,50 @@
+import './home.css';
+import React, { useState } from 'react';
+//import TextField from '@mui/material/TextField';
+//import Button from '@mui/material/Button';
+//import Card from '@mui/material/Card';
+//import CardContent from '@mui/material/CardContent';
+
+function Home() {
+  const [input, setInput] = useState('');
+  const [stickies, setStickies] = useState([]);
+
+  const handleInput = event => {
+    const {name, value} = event.target;
+    setInput(value);
+    console.log(name, value, input);
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("hiaofajlsdkfjasd")
+    console.log(stickies);
+    setStickies([...stickies, input]);
+    console.log(stickies);
+  }
+
+  return (
+    <div className="home">
+      {/* <div className="background-window"><GoogleCalendar></GoogleCalendar></div> */}
+      <div className="background-window">
+        <h2>feed</h2>
+        {/*<form class="submission" onSubmit={handleSubmit}>
+          <TextField class="textfield" placeholder="type a message" onChange={handleInput} fullWidth></TextField>
+          <Button class="stickybutton" type="submit" variant="contained" color="success">submit</Button>
+          </form>*/}
+        <div class="stickies">
+          {stickies.toReversed().map((sticky) => (
+            {/*<Card class="sticky">
+              <CardContent>
+                {sticky}
+              </CardContent>
+            </Card>
+          */}
+          ))} 
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
---
+++ irvinehacks/src/nav/nav.js
@@ -0,0 +1,17 @@
+import './nav.css';
+
+
+function Nav() {
+  return (
+    <div className="nav">
+        <div className="logo-container">
+            <img className="logo-pic" src="../logo.svg" alt="A perfectly generic logo"></img>
+            <div className="zotzen">
+                  zotzen
+              </div>
+    </div>
+    </div>
+  );
+}
+
+export default Nav;
