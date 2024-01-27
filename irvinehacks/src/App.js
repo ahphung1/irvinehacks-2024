1dda1318c985239e883fcd9e2d53ebd053265ce4
Xavier <xachuen@gmail.com>
2024-01-27 10:24:11 -0800

    deleted and added test stuff

--- irvinehacks/src/App.js
+++ irvinehacks/src/App.js
@@ -4,11 +4,32 @@ import Home from './home/home';
 
 function App() {
   return (
+<<<<<<< Updated upstream
     <div className="root">
       <Nav></Nav>
       <Home></Home>
+=======
+    <div className="App">
+      <Title></Title>
+        <p>
+          My website is running!
+        </p>
+>>>>>>> Stashed changes
     </div>
   );
 }
 
+<<<<<<< Updated upstream
 export default App;
+=======
+function Title() {
+  const Title = "Title!"
+  return (
+    <div className ="Title">
+      <h1>{Title}</h1>
+    </div>
+  )
+}
+
+export default App;
+>>>>>>> Stashed changes
