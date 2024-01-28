5599fd3504faead0418e86b749384798c9f343ce
Aaron Phung <user.name>
2024-01-27 22:17:38 -0800

    added functionality for expiring soon section

--- irvinehacks/src/App.js
+++ irvinehacks/src/App.js
@@ -13,6 +13,14 @@ function isExpired(item) {
   return date.getTime() < Date.now();
 }
 
+function isExpiring(item) {
+  if (!Date.parse(item.expiration_date)) {
+    return false;
+  }
+  const date = new Date(item.expiration_date);
+  return (date.getTime() - Date.now()) / (1000 * 60 * 60 * 24) < 7;
+}
+
 function App() {
   const [items, setItems] = useState([
       {
@@ -34,16 +42,36 @@ function App() {
 
   const [expiredItems, setExpiredItems] = useState([])
 
-  function updateItems(newItems) {
+  const [expiringItems, setExpiringItems] = useState([])
 
-    while (newItems.length > 0 && isExpired(newItems[0]))
-    {
+  function updateExpiringItems(toExpiring) {
+    var newItems = [...expiringItems, ...toExpiring]
+    console.log('expr')
+    console.log(newItems)
+    while (newItems.length > 0 && isExpired(newItems[0])) {
       setExpiredItems([...expiredItems, newItems[0]]);
       newItems.shift();
     }
-    console.log('exp')
-    console.log(expiredItems);
-    console.log(newItems);
+    setExpiringItems(newItems);
+  }
+
+  function updateItems(newItems) {
+    var toExpire = []
+    var toExpiring = []
+
+    while (newItems.length > 0 && isExpired(newItems[0]))
+    {
+      toExpire.push(newItems[0])
+      newItems.shift();
+    }
+    while (newItems.length > 0 && isExpiring(newItems[0]))
+    {
+      toExpiring.push(newItems[0])
+      console.log(toExpiring)
+      newItems.shift();
+    }
+    setExpiredItems([...expiredItems, ...toExpire])
+    updateExpiringItems(toExpiring);
     setItems(newItems);
    
   }
@@ -51,7 +79,7 @@ function App() {
   return (
     <div className="root">
       <Nav items={items}/>
-      <Middle items={expiredItems} setItems={setExpiredItems}/>
+      <Middle expired={expiredItems} setExpired={setExpiredItems} expiring={expiringItems} setExpiring={setExpiringItems}/>
       <Home items={items} setItems={(i) => updateItems(i)}/>
     </div>
   );
--- irvinehacks/src/components/pantry-storage/inc-expiration/inc-expiration.js
+++ irvinehacks/src/components/pantry-storage/inc-expiration/inc-expiration.js
@@ -1,24 +1,30 @@
 import './inc-expiration.css';
+import DeleteIcon from '@mui/icons-material/Delete';
+import IconButton from '@mui/material/IconButton';
 
-function Expiring() {
+function Expiring({items, setItems}) {
+  const handleDelete = (item) => {
+      let newItems = items.filter(i => i.id !== item.id)
+      setItems([...newItems,
+      ]);
+  }
   return (
-      <div className="contain">
-          <ExpiredItem text = "XX/XX/XXXX"/>
-          <ExpiredItem text = "XX/XX/XXXX"/>
-          <ExpiredItem text = "XX/XX/XXXX"/>
-          <ExpiredItem text = "XX/XX/XXXX"/>
-          <ExpiredItem text = "XX/XX/XXXX"/>
-          <ExpiredItem text = "XX/XX/XXXX"/>
-          <ExpiredItem text = "XX/XX/XXXX"/>
+      <div className="container">
+        {items.map((item) => (
+          <div>
+            <IconButton type="delete" className = "test" onClick={event => handleDelete(item)}><DeleteIcon/></IconButton>
+            <ExpiringItem name={item.name} expiration_date={item.expiration_date}/>
+          </div>
+        ))}
       </div>
   )
 }
 
-function ExpiredItem(props) {
+function ExpiringItem(props) {
   return (
     <div className="expiring-item">
-      <p>Item</p>
-      <div className="expired date">{props.text}</div>
+      <p>{props.name}</p>
+      <div className="expiration-date">{props.expiration_date}</div>
     </div>
   )
 }
