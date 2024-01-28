b8af179d13cf52f29c65d61a0b4372430e689fbd
yirenx1 <emmelinexu23@gmail.com>
2024-01-28 09:19:01 -0800

    Update nav.js
    customize usr name

--- irvinehacks/src/nav/nav.js
+++ irvinehacks/src/nav/nav.js
@@ -1,16 +1,35 @@
 import './nav.css';
+import React, { useState } from 'react';
 import ProfilePicture from './profilepicture.png';
 import RecipeBar from '../components/pantry-storage/recipes/recipes.js';
 
 function Nav({ items }) {
-  const userName = "Steven";
+  const [userName, setUserName] = useState('');
+  const handleNameChange = (event) => {
+    setUserName(event.target.value);
+  };
+
+  const handleNameSubmit = (event) => {
+    event.preventDefault(); 
+    alert(`Hello ${userName}!`); 
+  };
+
   return (
     <div className="navcontainer">
-      <div className = "nav">
-        <div className= "welcome-text">
-          <h2>Welcome {userName}!</h2>
-          </div>
+    <div className="nav">
+      <div className="welcome-text">
+        <form onSubmit={handleNameSubmit}>
+          <input
+            type="text"
+            placeholder="What is your name?"
+            value={userName}
+            onChange={handleNameChange}
+          />
+          <button type="submit">Submit</button>
+        </form>
+        {userName && <h2>Welcome {userName}!</h2>}
       </div>
+    </div>
       <div>
         <img className = "pfp" src={ProfilePicture} alt="Avatar"></img>
       </div>
